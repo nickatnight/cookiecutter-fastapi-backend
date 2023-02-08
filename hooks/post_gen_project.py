11ad7ceec69858f49bda4bde93c8f79d88da50c4
nickatnight <kay.muneee@gmail.com>
2023-02-08 00:10:51 -0800

    fix: missing env file due to git ignore

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -19,6 +19,12 @@ def remove_file(filepath: str) -> None:
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
+def rename_file(old: str, new: str):
+    in_ = os.path.join(PROJECT_DIRECTORY, old)
+    out_ = os.path.join(PROJECT_DIRECTORY, new)
+    os.rename(in_, out_)
+
+
 if "{{ cookiecutter.include_example_api}}" == "no":
     print("Removing example api files...")
     for p in MEME_FILE_PATHS:
@@ -29,3 +35,6 @@ if "{{ cookiecutter.deployments }}" == "no":
     print("Removing deployment files...")
     for p in DEPLOYMENT_FILES:
         remove_file(p)
+
+
+rename_file(".env_example", ".env")
