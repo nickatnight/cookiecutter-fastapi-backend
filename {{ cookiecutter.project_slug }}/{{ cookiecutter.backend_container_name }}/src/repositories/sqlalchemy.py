0c1aa0ede254640e1633abb30e16f378d44189fe
nickatnight <kay.muneee@gmail.com>
2023-02-17 01:10:44 -0800

    fix: yml formatting

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/repositories/sqlalchemy.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/repositories/sqlalchemy.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Optional, Type, TypeVar, List
+from typing import List, Optional, Type, TypeVar
 
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import SQLModel, select
