fe1cde4a1917e0cc94b09add04e6267942014580
nickatnight <kay.muneee@gmail.com>
2023-02-17 00:51:27 -0800

    fix: non yaml lint

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/repositories/sqlalchemy.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/repositories/sqlalchemy.py
@@ -51,7 +51,7 @@ class SQLAlchemyRepository(IRepository[ModelType]):
 
         if not scalar:
             raise ObjectNotFound(f"Object with [{kwargs}] not found.")
-    
+
         return scalar
 
     async def update(self, obj_current: ModelType, obj_in: ModelType) -> ModelType:
@@ -75,7 +75,7 @@ class SQLAlchemyRepository(IRepository[ModelType]):
 
         await self.db.delete(obj)
         await self.db.commit()
-    
+
     async def all(
         self,
         skip: int = 0,
