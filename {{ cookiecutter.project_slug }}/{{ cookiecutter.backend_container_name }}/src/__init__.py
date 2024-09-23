1c20bf0c002f60fd47964340f2d0169d558386e4
nickatnight <kay.muneee@gmail.com>
2024-09-22 17:15:17 -0700

    feat: add celery test options and conditionals [#48]

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/__init__.py
@@ -1,4 +1,4 @@
-from .celery import app as celery_app
+{%- if cookiecutter.use_celery == "yes" %}from .celery import app as celery_app
 
 
-__all__ = ("celery_app",)
+__all__ = ("celery_app",){%- endif %}
