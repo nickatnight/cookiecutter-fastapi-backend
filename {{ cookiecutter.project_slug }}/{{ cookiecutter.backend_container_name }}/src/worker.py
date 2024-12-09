0ba5353cddca8b0bdcfb5603600c330ea1b5d1e2
longniao <longniao@gmail.com>
2024-12-09 13:14:00 -0600

    Fix worker path and connect Celery with Redis (#55)
    * fix worker path
    
    * connect celery with redis
    
    * return type annotation
    
    * fix import module name

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/worker.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/worker.py
@@ -1,6 +1,9 @@
 from celery import Celery
 
+from src.api.deps import get_redis_url
 
-celery_app = Celery(__name__)
+
+redis_url = get_redis_url()
+celery_app = Celery(__name__, broker=redis_url, backend=redis_url)
 # add more tasks roots here
 celery_app.autodiscover_tasks(["src.core.tasks"], related_name="tasks", force=True)
