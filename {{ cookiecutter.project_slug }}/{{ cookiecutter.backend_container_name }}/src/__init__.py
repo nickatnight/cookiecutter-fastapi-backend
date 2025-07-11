bea1d49cf737e4313f51535a1b8ee5c47faeb435
nickatnight <kay.muneee@gmail.com>
2025-07-11 06:17:21 -0700

    fix: newline again [#47]

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/__init__.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/__init__.py
@@ -1,4 +1,5 @@
-{%- if cookiecutter.use_celery == "yes" %}from .worker import celery_app
+{%- if cookiecutter.use_celery == "yes" %}
+from .worker import celery_app
 
 
 __all__ = ("celery_app",){%- endif %}
