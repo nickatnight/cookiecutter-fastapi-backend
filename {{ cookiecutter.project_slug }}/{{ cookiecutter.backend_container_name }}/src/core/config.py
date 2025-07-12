2a5df5bab5ca80678e5f8f11be2b9f0c5a34c09a
nickatnight <kay.muneee@gmail.com>
2025-07-11 18:36:31 -0700

    chore: moar lint [#61]

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/core/config.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/core/config.py
@@ -54,7 +54,7 @@ class Settings(BaseSettings):
             path=f"{values.data.get('POSTGRES_DB') or ''}",
         ).unicode_string()
 
-    {%- if cookiecutter.use_celery == "yes" %}
+    {%- if cookiecutter.use_celery == "yes" -%}
     @field_validator("REDIS_URL", mode="before")
     @classmethod
     def build_redis_connection(cls, v: Optional[str], values: ValidationInfo) -> Any:
