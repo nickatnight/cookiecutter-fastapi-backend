c31f91e880824e9bde2cc00932343dc4007109c6
nickatnight <kay.muneee@gmail.com>
2025-07-20 16:19:02 -0700

    chore: drop fastapi-cache2 [#83]

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/main.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.backend_container_name }}/src/main.py
@@ -3,9 +3,6 @@ import logging
 import sentry_sdk{% endif %}
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-{%- if cookiecutter.use_celery == "yes" %}
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend{%- endif %}
 
 from src.api import routes
 {%- if cookiecutter.use_celery == "yes" %}
@@ -35,9 +32,6 @@ app = FastAPI(
 
 def on_startup() -> None:
     add_postgresql_extension()
-    {%- if cookiecutter.use_celery == "yes" %}
-    redis_client = get_redis_client()
-    FastAPICache.init(RedisBackend(redis_client), prefix="fastapi-cache"){%- endif %}
     {%- if cookiecutter.use_sentry == "yes" %}
     if settings.SENTRY_DSN:
         sentry_sdk.init(
